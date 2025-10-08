06a270d9545fb2cbd7217e6efd54d3e9162c456d
TiagoCosta79817 <a79817@ualg.pt>
2025-10-08 18:27:43 +0100

    Revert "."
    This reverts commit 6943343e8df59e50032a01a976ed53577235203f.

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,15 +35,11 @@ async function startServer() {
 
   // app.use(express.static(path.join(__dirname, '../public')));
 
-  app.set('trust proxy', 1);
   app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-		secure: true,   // obrigatório em HTTPS
-    	sameSite: 'lax',
-		maxAge: 24*60*60*1000}
+    cookie: { secure: 'auto' }
   }));
 
   setupSteamAuth(app);
